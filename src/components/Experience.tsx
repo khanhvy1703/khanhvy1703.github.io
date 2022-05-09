22bacc19c950fa09f0df393a5d757e9409f0d9bc
Ngoc (Victoria) Le <58549806+khanhvy1703@users.noreply.github.com>
2022-05-09 18:33:37 -0400

    add experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -11,7 +11,9 @@ export default function About() {
   return (
     <div id='experience' style={{ backgroundColor: 'rgba(95, 158, 160, 0.3)' }}>
       <Container>
-        <p className='main-header main-color-font'>Work Experience & Volunteer</p>
+        <p className='main-header main-color-font'>
+          Work Experience & Volunteer
+        </p>
         <ColoredLine height={5} color='black' width='30%' />
         <VerticalTimeline>
           <VerticalTimelineElement
@@ -23,8 +25,22 @@ export default function About() {
               fontSize: '18px',
             }}
             contentArrowStyle={{ borderRight: '7px solid rgb(112, 166, 202)' }}
-            date='January 2022 - present'
-            iconStyle={{ background: 'rgb(112, 166, 202)'}}
+            date='May 2022 - Present'
+            iconStyle={{ background: 'rgb(112, 166, 202)' }}
+          >
+            <h3 className='vertical-timeline-element-title'>
+              Teaching Assisstant
+            </h3>
+            <h2 className='vertical-timeline-element-subtitle'>
+              Khoury College of Computer Science
+            </h2>
+            <h2 className='vertical-timeline-element-subtitle'>Boston, MA</h2>
+            <p>Teaching Assistant for CS3000 - Algorithm and Data Structure</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className='vertical-timeline-element--work'
+            date='January 2022 - May 2022'
+            iconStyle={{ background: 'rgb(71,89,101)' }}
           >
             <h3 className='vertical-timeline-element-title'>
               Teaching Assisstant
@@ -38,38 +54,34 @@ export default function About() {
           <VerticalTimelineElement
             className='vertical-timeline-element--work'
             date='September 2021 - February 2022'
-            iconStyle={{ background: 'rgb(71,89,101)'}}
+            iconStyle={{ background: 'rgb(71,89,101)' }}
           >
             <h3 className='vertical-timeline-element-title'>
               Build Studio Software Engineer
             </h3>
-            <h2 className='vertical-timeline-element-subtitle'>
-              Generate
-            </h2>
+            <h2 className='vertical-timeline-element-subtitle'>Generate</h2>
             <h2 className='vertical-timeline-element-subtitle'>Boston, MA</h2>
             <p>Student club - member</p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className='vertical-timeline-element--work'
             date='July 2021 - December 2021'
-            iconStyle={{ background: 'rgb(71,89,101)'}}
+            iconStyle={{ background: 'rgb(71,89,101)' }}
           >
             <h3 className='vertical-timeline-element-title'>
               SDK Engineer Coop
             </h3>
+            <h2 className='vertical-timeline-element-subtitle'>Skillz, Inc</h2>
             <h2 className='vertical-timeline-element-subtitle'>
-              Skillz, Inc
+              Portland, OR (Remote)
             </h2>
-            <h2 className='vertical-timeline-element-subtitle'>Portland, OR (Remote)</h2>
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className='vertical-timeline-element--work'
             date='January 2020 - March 2020'
-            iconStyle={{ background: 'rgb(71,89,101)'}}
+            iconStyle={{ background: 'rgb(71,89,101)' }}
           >
-            <h3 className='vertical-timeline-element-title'>
-              Mentor
-            </h3>
+            <h3 className='vertical-timeline-element-title'>Mentor</h3>
             <h2 className='vertical-timeline-element-subtitle'>
               Roxbury Robotics
             </h2>
@@ -78,7 +90,7 @@ export default function About() {
           <VerticalTimelineElement
             className='vertical-timeline-element--work'
             date='September 2019 - December 2019'
-            iconStyle={{ background: 'rgb(71,89,101)'}}
+            iconStyle={{ background: 'rgb(71,89,101)' }}
           >
             <h3 className='vertical-timeline-element-title'>
               Community Service
